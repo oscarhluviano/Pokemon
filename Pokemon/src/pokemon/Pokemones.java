0ab57d9a2df7ac5d4c718350f1403a8eec8a94d3
oscarhluviano <oscarhluviano@gmail.com>
2017-04-21 19:09:15 -0500

    Clase pokemones

---
+++ Pokemon/src/pokemon/Pokemones.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokemon;
+
+/**
+ *
+ * @author Oscar Hernandez
+ */
+public class Pokemones {
+    public String nombre = "";
+    public int vida;
+    public int energia;
+    public String ataque ;
+    public String Ataqueesp;
+}
